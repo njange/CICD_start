a577f89daaed01c2ebc7395b7674f7e3ed545f12
njange <jamesnjange80@gmail.com>
2024-11-09 15:31:11 +0300

    Update GitHub Actions workflow for Docker

---
+++ react-frontend/src/App.js
@@ -0,0 +1,17 @@
+// react-frontend/src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello from React frontend!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
